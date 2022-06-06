44a6d5b4871ca34bd931b15b37d61cf9017152ed
HieuViper <mr.hieu2491@gmail.com>
2022-06-06 17:22:42 +0700

    add animate scrolling and bug parallax

--- README.md
+++ README.md
@@ -1,13 +1,17 @@
-TailwindCSS Coffee Style by DevHieu
+brTailwindCSS Coffee Style by DevHieu
 
-Requirements
-nodejs >= v14.16.0
+Requirements<br />
+nodejs >= v14.16.0<br />
 yarn >= v1.22.18
 
 Clone project and run test.
-$ git clone https://git_url_clone <project_dir>
-$ cd <project_dir>
-$ yarn build:css
-$ yarn start:dev
+<br />
+> git clone https://git_url_clone <project_dir> 
+<br />
+> cd <project_dir>
+<br />
+> yarn build:css
+<br />
+> yarn start:dev
 
 Thanks for watching!
--- public/main.js
+++ public/main.js
@@ -2,18 +2,17 @@ const topMenu = document.getElementById('top-menu')
 const toogleTopMenuIcon = document.getElementById('toggle-top-menu-icon')
 
 
-document.addEventListener('click' , (e) => {
-  if(toogleTopMenuIcon.contains(e.target)){
+document.addEventListener('click', (e) => {
+  if (toogleTopMenuIcon.contains(e.target)) {
     //Click toggle top menu icon
     topMenu.classList.toggle('top-menu-expanded')
     topMenu.classList.toggle('hidden')
   }
-  else{
+  else {
     //click outside top menu icon
-    if(topMenu.classList.contains('top-menu-expanded')){
+    if (topMenu.classList.contains('top-menu-expanded')) {
       topMenu.classList.remove('top-menu-expanded')
       topMenu.classList.add('hidden')
     }
   }
 })
-
